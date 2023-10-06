69859ad240c615600d0df0161a92f6d8aee4a523
Priit P. <priit.printsmann@tktk.ee>
2023-10-06 22:51:10 +0300

    add week9 code

---
+++ Week9ListsTest/Program.cs
@@ -0,0 +1,20 @@
+﻿//string folderPath = @"C:\Massiiv";
+//string fileName = "shoppingList.txt";
+//if (File.Exists(Path.Combine(folderPath, fileName)))
+//{
+//    Console.WriteLine($"File {fileName} exists.");
+//}
+//else
+//{
+//    Console.WriteLine($"File {fileName} does not exist.");
+//}
+
+static
+void
+ DisplayDataFromFile(List<string> someList)
+{
+    foreach (string line in someList)
+    {
+        Console.WriteLine(line);
+    }
+}
